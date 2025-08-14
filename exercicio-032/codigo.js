a015ea7ad1150bb13aecc65191ab4b39219f71b2
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2025-08-14 12:07:50 -0300

    update

--- exercicio-032/codigo.js
+++ exercicio-032/codigo.js
@@ -1,52 +1,68 @@
- /* Descrição do exercício:
+/* Descrição do exercício:
 
- Usando um laço do-while, crie um jogo de "Pedra, papel e tesoura" contra o computador. 
+Usando um laço do-while, crie um jogo de "Pedra, papel e tesoura" contra o computador. 
  
- Descrição: Neste exercício, você vai simular um jogo de "Pedra, papel e tesoura" contra o computador. Para simplificar, vamos supor que a escolha do usuário é sempre "pedra".
+Descrição: Neste exercício, você vai simular um jogo de "Pedra, papel e tesoura" contra o computador. Para simplificar, vamos supor que a escolha do usuário é sempre "pedra".
 
- */
+*/
 
- const inputObjeto = document.getElementById('objeto')
- const btnJogar = document.querySelector('button.btnJogar')
- const btnNovaJogada = document.querySelector('button.btnNovaJogada')
- const resposta = document.querySelector('div.resposta')
+const inputObjeto = document.getElementById('objeto')
+const btnJogar = document.querySelector('button.btnJogar')
+const btnNovaJogada = document.querySelector('button.btnNovaJogada')
+const resposta = document.querySelector('div.resposta')
 
- function jogar() {
-    if (inputObjeto.value === '') {
-        alert('Digite um número para jogar.')
+function jogar() {
+
+    const objetos = ['pedra', 'papel', 'tesoura']
+
+    const jogadaUsuario = inputObjeto.value.trim().toLowerCase()
+
+    if (jogadaUsuario === '') {
+        alert('Digite um objeto para jogar.')
         inputObjeto.focus()
         return
     }
 
-    const jogadaUsuario = Number(inputObjeto.value)
-
-    if (objeto < 1 || objeto > 3) {
-        alert('Digite valores entre 1 e 3 para jogar.')
+    // o "includes" verifica se o array possui o valor digitado
+    if (!objetos.includes(jogadaUsuario)) {
+        alert('Digite apenas: pedra, papel ou tesoura.')
         inputObjeto.value = ''
         inputObjeto.focus()
         return
     }
 
-    const jodadaComputador = 1
-
     let resultado = ''
+    let jogadaComputador = ''
 
-    // 1 = Pedra | 2 = Papel | 3 = Tesoura
-    if (jogadaUsuario === 1 && jodadaComputador === 3) {
-        resultado = 'Você ganhou!'
-    } else if (jogadaUsuario === 3 && jodadaComputador === 2) {
-        resultado = 'Você ganhou!'
-    } else if (jogadaUsuario === 2 && jodadaComputador === 1) {
-        resultado = 'Você ganhou!'
-    } else if (jogadaUsuario === jodadaComputador) {
-        resultado = 'Deu empate. Jogue de novo!'
-    } else {
-        resultado = 'Você perdeu miserável!'
-    }
+    do {
+        jogadaComputador = objetos[Math.floor(Math.random() * objetos.length)]
 
-    resposta.innerHTML = `<p>${resultado} | Computador jogou ${jodadaComputador}</p>`
+        if (jogadaUsuario === jogadaComputador) {
+            resultado = 'Empate!'
+        } else if (
+            (jogadaUsuario === 'pedra' && jogadaComputador === 'tesoura') ||
+            (jogadaUsuario === 'papel' && jogadaComputador === 'pedra') ||
+            (jogadaUsuario === 'tesoura' && jogadaComputador === 'papel')
+        ) {
+            resultado = 'Você ganhou!'
+        } else {
+            resultado = 'Você perdeu miserável!'
+        }
+    } while (resultado === 'Empate!')
 
- }
+    resposta.innerHTML = `<p>${resultado} | Computador jogou <strong>${jogadaComputador}</strong></p>`
 
+    btnJogar.style.display = 'none'
+    btnNovaJogada.style.display = 'inline-block'
 
+}
 
+function novaJogada() {
+    inputObjeto.value = ''
+    inputObjeto.focus()
+
+    btnJogar.style.display = 'inline-block'
+    btnNovaJogada.style.display = 'none'
+
+    resposta.innerHTML = ''
+}
