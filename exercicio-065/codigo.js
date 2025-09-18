516507d132735fe3d2191589d269c3af1e2afd0b
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2025-09-18 11:14:09 -0300

    update

--- exercicio-065/codigo.js
+++ exercicio-065/codigo.js
@@ -1,10 +1,10 @@
- /* Descrição do exercício:
+/* Descrição do exercício:
 
 Escreva uma função que aceite um array de números e uma função de callback e retorne a soma de todos os números do array após a aplicação da função de callback. 
 
-Descrição:  Neste exercício, você deve criar uma função de alta ordem que receba um array de números e uma função de callback como argumentos, e retorne a soma de todos os números do array depois de aplicar a função de callback a cada número.
+Descrição: Neste exercício, você deve criar uma função de alta ordem que receba um array de números e uma função de callback como argumentos, e retorne a soma de todos os números do array depois de aplicar a função de callback a cada número.
 
- */
+*/
 
 const inputNumero = document.getElementById('numero')
 const btnAdicionarNumero = document.querySelector('button.btnAdicionarNumero')
@@ -32,25 +32,52 @@ function adicionarNumero() {
 }
 
 
-function chamarCallback() {
+function chamarCallback(callback) {
     if (listaNumeros.length < 2) {
         alert('Adicione mais números à lista.')
         inputNumero.focus()
         return
     }
 
-    callbackSomar()
+    callback(listaNumeros)
+
 }
 
 
-function callbackSomar() {
+function callbackSomar(listaNumerosPassada) {
 
     let somaNumeros = 0
 
-    for (let i = 0; i < listaNumeros.length; i++) {
-        somaNumeros += listaNumeros[i]
+    for (let i = 0; i < listaNumerosPassada.length; i++) {
+        somaNumeros += listaNumerosPassada[i]
     }
 
     resposta.innerHTML = `<p>Soma da lista: ${somaNumeros}</p>`
 
+    inputNumero.disabled = true
+
+    btnAdicionarNumero.style.display = 'none'
+    btnChamarCallback.style.display = 'none'
+    btnNovaSoma.style.display = 'inline-block'
 }
+
+
+function novaSoma() {
+    inputNumero.disabled = false
+    inputNumero.value = ''
+    inputNumero.focus()
+
+    btnAdicionarNumero.style.display = 'inline-block'
+    btnChamarCallback.style.display = 'inline-block'
+    btnNovaSoma.style.display = 'none'
+
+    listaNumeros.length = 0
+
+    resposta.innerHTML = ''
+}
+
+
+// Eventos dos botões (em vez de colocar "onclick" direto no HTML)
+btnAdicionarNumero.addEventListener('click', adicionarNumero)
+btnChamarCallback.addEventListener('click', () => chamarCallback(callbackSomar))
+btnNovaSoma.addEventListener('click', novaSoma)
