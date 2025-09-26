8a31cf5376a481ad23a96bdc3244a9d3e1574103
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2025-09-26 12:08:49 -0300

    update

---
+++ exercicio-070/codigo.js
@@ -0,0 +1,50 @@
+ /* Descrição do exercício:
+
+Escreva uma função de alta ordem que aceite um array de funções de callback e um valor, e retorne um array de resultados de aplicar cada função de callback ao valor. 
+
+Descrição: Neste exercício, você deve criar uma função de alta ordem que receba um array de funções de callback e um valor como argumentos, e retorne um array que contenha os resultados de aplicar cada função de callback ao valor.
+
+ */
+
+const inputTexto = document.getElementById('texto')
+const btnTransformar = document.querySelector('button.btnTransformar')
+const btnNovoTexto = document.querySelector('button.btnNovoTexto')
+const resposta = document.querySelector('div.resposta')
+
+function transformarTexto() {
+    if (inputTexto.value.trim() === '') {
+        alert('Digite um texto para processar.')
+        inputTexto.focus()
+        return
+    }
+
+    const texto = inputTexto.value
+
+    let resultado = texto
+
+    callbacks.forEach(callback => {
+        resultado = callback(resultado) // Encadeamento das funções de callback
+    })
+
+    resposta.innerHTML = `<p>${resultado}</p>`
+}
+
+
+// Array de funções de callback
+const callbacks = [textoMaiusculo, textoInvertido, textoSemEspacos]
+
+
+// Funções de callback
+function textoMaiusculo(textoPassado) {
+    return textoPassado.toUpperCase()
+}
+
+function textoInvertido(textoPassado) {
+    return textoPassado.split('').reverse().join('')
+}
+
+function textoSemEspacos(textoPassado) {
+    // Essa função varre a string e remove todos os espaços em branco ("/\s+/g" é uma expressão regular), deixando o texto colado.
+    return textoPassado.replace(/\s+/g, '') 
+}
+
