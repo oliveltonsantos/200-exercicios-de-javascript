96784106e206758d54a1459224fa52d38ab468ce
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2025-10-09 11:20:43 -0300

    update

--- exercicio-075/codigo.js
+++ exercicio-075/codigo.js
@@ -1,10 +1,12 @@
- /* Descrição do exercício:
+/* Descrição do exercício:
 
 Escreva uma função de alta ordem que aceite um array de funções de callback e um valor, e retorne um novo array que contém os resultados de aplicar a função de callback ao valor, na ordem inversa. 
 
 Descrição: Neste exercício, você deve criar uma função de alta ordem que receba um array de funções de callback e um valor como argumentos, e retorne um novo array que contenha os resultados de aplicar cada função de callback ao valor, na ordem inversa.
 
- */
+Observação: Teste com 11205.81 (valor final) e verifique se retorna 10000.00 (valor inicial).
+
+*/
 
 const inputValorFinal = document.getElementById('valorFinal')
 const btnProcessarInvestimento = document.querySelector('button.btnProcessarInvestimento')
@@ -30,50 +32,63 @@ function processarInvestimento() {
 
     let resultado = reverterAjustesFinanceiros(arrayDeCallbacks, valorFinal)
 
-    resposta.innerHTML = `<p>Valor final: ${resultado}</p>`
+    resposta.innerHTML = `<p>Valor inicial: R$${resultado.toFixed(2)}</p>`
 
+    inputValorFinal.disabled = true
+
+    btnProcessarInvestimento.style.display = 'none'
+    btnprocessarNovoInvestimento.style.display = 'inline-block'
 }
 
 
-
 // Função de alta ordem
-function reverterAjustesFinanceiros(arrayPassado, valorPassado) {
+function reverterAjustesFinanceiros(arrayCallbacks, valorPassado) {
 
-    for (let i = 0; i < arrayPassado.length; i++) {
-        arrayPassado.forEach(array => {
-            array[i] = valorPassado
-        })
+    let v = valorPassado
+
+    for (let i = arrayCallbacks.length - 1; i >= 0; i--) {
+        v = arrayCallbacks[i](v)
     }
 
+    return v
 }
 
 
 // Array de funções de callback
-
-let arrayDeCallbacks = [juros, impostos, administracao, bonus]
+const arrayDeCallbacks = [desfazerJuros, desfazerImpostos, desfazerAdministracao, desfazerBonus]
 
 
 // Funções de callback
-
-function juros(valorEnviado) {
+function desfazerJuros(valorEnviado) {
     // +5%
-    return valorEnviado + ((valorEnviado * 5) / 100)
+    return valorEnviado / 1.05
 }
 
-function impostos(valorEnviado) {
+function desfazerImpostos(valorEnviado) {
     // -2%
-    return valorEnviado - ((valorEnviado * 2) / 100)
+    return valorEnviado / 0.98
 }
 
-function administracao(valorEnviado) {
+function desfazerAdministracao(valorEnviado) {
     // -1%
-    return valorEnviado - ((valorEnviado * 1) / 100)
+    return valorEnviado / 0.99
 }
 
-function bonus(valorEnviado) {
+function desfazerBonus(valorEnviado) {
     // +10%
-    return valorEnviado + ((valorEnviado * 10) / 100)
+    return valorEnviado / 1.10
 }
 
 
+// Fazer novo cálculo
+function processarNovoInvestimento() {
+    inputValorFinal.disabled = false
+    inputValorFinal.value = ''
+    inputValorFinal.focus()
+
+    btnProcessarInvestimento.style.display = 'inline-block'
+    btnprocessarNovoInvestimento.style.display = 'none'
+
+    resposta.innerHTML = ''
+}
 
